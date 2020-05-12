a37e56a1e1d0aa278c23bb0233a254fc58dae0ae
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-05-12 11:57:27 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,13 @@
+
+class myclass{
+ public static void main(string[] args) 
+  {
+    int[] arr ={6,7,18,98};
+    int sum =0;
+    for(int i=0;i<=arr.length;i++)
+    {
+       sum+=arr[i]; 
+    }
+    System.out.println(sum); 
+}
+
