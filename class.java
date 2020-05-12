41aabc51f0cb68b43b6bfdb6f20acc56877f006e
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-05-12 12:05:49 +0530

    Create class.java

---
+++ class.java
@@ -0,0 +1,16 @@
+public class animal{
+  void bark(){
+      System.out.println("woof woof"); 
+  }
+
+}
+
+class myclass{
+   public static void main(string[] args) 
+    { 
+      animal dog = new animal(); 
+      dog.bark(); 
+
+   }
+
+}
