1e0fc0d6c38438a8e468aa8b07ac96efe8c01272
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-05-12 11:51:33 +0530

    Create input data.java

---
+++ input data.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class myclass
+{ 
+   public static void main(string[] args)
+   {
+     Scanner myvar = new Scanner(System.in); 
+     System.out.println(myvar.nextLine()); 
+  } 
+} 
+
+
