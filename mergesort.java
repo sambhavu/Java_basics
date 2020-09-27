ea1b73b6bfb3ce55017996fcb0eba38307eb2103
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-09-26 21:37:38 -0400

    Create mergesort.java

---
+++ mergesort.java
@@ -0,0 +1,16 @@
+public class Merge { 
+  //...
+  private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) 
+  { 
+    
+    if(hi <= lo) return ;
+    int mid = lo + (hi - lo)/2; 
+    sort(a, aux, lo, mid); 
+    sort(a,aux, mid + 1, hi); 
+    merge(a,aux,low,mid,hi);
+    } 
+    
+   
+//...
+
+} 
