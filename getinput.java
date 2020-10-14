2903c8aaf5ded6d27aa18774513250366090db26
simpledaddy <30390294+simpledaddy@users.noreply.github.com>
2020-10-13 21:56:54 -0400

    Create getinput.java

---
+++ getinput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; 
+
+class MyClass { 
+  public static void main(String[] args) 
+  {
+      Scanner myVar = new Scanner(System.in); 
+      System.out.println(myVar.nextLine();
+      
+      
+   }
+   
+   
+} 
+
